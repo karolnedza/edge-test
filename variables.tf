46a7d454968c8e98345166ac67ba85d4d9ccfc89
karolnedza <37583943+karolnedza@users.noreply.github.com>
2025-05-06 14:26:05 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "ctrl_ip" {
+  type        = string
+}
+
+variable "passwd" {
+  type        = string
+}
+
+variable "my-access-key" {
+  type        = string
+}
+
+variable "my-secret-key" {
+  type        = string
+}
